61305f23d0540d1a7e0df88beeb52084ef8d0771
meet9614 <meetkumarsarkar9614@gmail.com>
2026-03-13 17:23:03 +0530

    Added JWT authentication and GET/DELETE profile APIs

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -20,3 +20,32 @@ def update_profile(user_id: int, data: ProfileUpdate, db: Session = Depends(get_
     db.commit()
 
     return {"message": "Profile updated"}
+
+@router.get("/profile/{user_id}")
+def get_profile(user_id: int, db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.user_id == user_id).first()
+
+    if not user:
+        return {"message": "User not found"}
+
+    return {
+        "user_id": user.user_id,
+        "name": user.name,
+        "email": user.email,
+        "bio": user.bio,
+        "phone": user.phone
+    }
+
+@router.delete("/profile/delete/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+
+    user = db.query(User).filter(User.user_id == user_id).first()
+
+    if not user:
+        return {"message": "User not found"}
+
+    db.delete(user)
+    db.commit()
+
+    return {"message": "User deleted successfully"}
